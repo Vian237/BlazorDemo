94aa589cdf3c15feae792033d9687b41e3da8f82
Vianney Ngaleu <vianneyngaleu@gmail.com>
2025-07-03 14:48:54 +0200

    added a Todo List

---
+++ BlazorDemo/Components/Pages/Todo.razor.cs
@@ -0,0 +1,19 @@
+﻿using BlazorDemo.Model;
+
+namespace BlazorDemo.Components.Pages
+{
+    public partial class Todo
+    {
+        private List<TodoItem> todoItems = new();
+        private string newTodo="";
+
+        private void AddTodo()
+        {
+            if (!string.IsNullOrWhiteSpace(newTodo))
+            {
+                todoItems.Add(new TodoItem { Title = newTodo });
+                newTodo = string.Empty;
+            }
+        }
+    }
+}
