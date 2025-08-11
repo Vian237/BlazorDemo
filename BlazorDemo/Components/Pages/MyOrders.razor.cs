d9875adab71cff560c1355db1ce098c2ebf43607
Vianney Ngaleu <vianneyngaleu@gmail.com>
2025-08-11 13:24:56 +0200

    Add My Orders page and enhance navigation

---
+++ BlazorDemo/Components/Pages/MyOrders.razor.cs
@@ -0,0 +1,15 @@
+﻿using BlazorDemo.Model;
+using System.Threading.Tasks;
+
+namespace BlazorDemo.Components.Pages
+{
+    public partial class MyOrders
+    {
+        public List<OrderWithStatus> OrdersWithStatus = new();
+
+        protected override async Task OnParametersSetAsync()
+        {
+            OrdersWithStatus = await HttpClient.GetFromJsonAsync<List<OrderWithStatus>>($"{NavigationManager.BaseUri}api/orders");
+        }
+    }
+}
