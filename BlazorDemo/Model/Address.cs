964a84f5cd217474d568451ac7d79340ce40197d
Vianney Ngaleu <vianneyngaleu@gmail.com>
2025-09-15 21:10:40 +0200

    Enhance validation messages and feedback in forms

--- BlazorDemo/Model/Address.cs
+++ BlazorDemo/Model/Address.cs
@@ -5,15 +5,15 @@ namespace BlazorDemo.Model
     public class Address
     {
         public int Id { get; set; }
-        [Required, MinLength(3), MaxLength(50)]
+        [Required, MinLength(3, ErrorMessage ="Please use a Name bigger than 3 letters."), MaxLength(50, ErrorMessage ="Please use a Name less than 100 letters.")]
         public string? Name { get; set; }
-        [Required, MinLength(5), MaxLength(100)]
+        [Required, MinLength(5, ErrorMessage = "Please use an Address bigger than 5 letters."), MaxLength(100, ErrorMessage = "Please use an Address less than 100 letters.")]
         public string? Street { get; set; }
-        [Required, MinLength(2), MaxLength(50)]
+        [Required, MinLength(2, ErrorMessage = "Please use a City bigger than 2 letters."), MaxLength(50, ErrorMessage = "Please use a City bigger less 50 letters.")]
         public string? City { get; set; }
-        [Required, MinLength(2), MaxLength(50)]
+        [Required, MinLength(2, ErrorMessage = "Please use a State bigger than 2 letters."), MaxLength(50, ErrorMessage = "Please use a State less than 50 letters.")]
         public string? State { get; set; }
-        [Required, RegularExpression(@"^([0-9]{5})$")]
+        [Required, RegularExpression(@"^([0-9]{5})$", ErrorMessage ="Please use a valid Postal code with five numbers.")]
         public string? ZipCode { get; set; }
     }
 }
