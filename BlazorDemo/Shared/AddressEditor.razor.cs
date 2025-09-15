cfd4180d51deb9b066e9e2d914cbec73a841b24b
Vianney Ngaleu <vianneyngaleu@gmail.com>
2025-09-15 18:38:28 +0200

    Refactor checkout process with new components

---
+++ BlazorDemo/Shared/AddressEditor.razor.cs
@@ -0,0 +1,26 @@
+﻿using BlazorDemo.Model;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorDemo.Shared
+{
+    public partial class AddressEditor
+    {
+        [Parameter]
+        public Address? Address { get; set; }
+
+        private ElementReference startName;
+
+        /// <summary>
+        /// Set focus to the first input element when the component is rendered
+        /// </summary>
+        /// <param name="firstRender"></param>
+        /// <returns></returns>        
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                await startName.FocusAsync();
+            }
+        }
+    }
+}
---
+++ BlazorDemo/Shared/OrderReview.razor.cs
@@ -0,0 +1,11 @@
+﻿using BlazorDemo.Model;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorDemo.Shared
+{
+    public partial class OrderReview
+    {
+        [Parameter]
+        public Order Order { get; set; }
+    }
+}
